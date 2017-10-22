7217cf3daa55767437bcca1b21a7d81ee25bfb38
Mahmoud Reyad <MahmoudReyad@fci.helwan.edu.eg>
2017-10-22 16:12:22 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  var chart = new CanvasJS.Chart("chartContainer", {
+  	animationEnabled: true,
+  	title: {
+
+  		text: "Our Company Achivment"
+  	},
+  	data: [{
+  		type: "pie",
+  		startAngle: 300,
+  		yValueFormatString: "##0.00\"%\"",
+  		indexLabel: "{label} {y}",
+  		dataPoints: [
+  			{y: 79.45, label: "Google"},
+  			{y: 7.31, label: "Bing"},
+  			{y: 7.06, label: "Baidu"},
+  			{y: 4.91, label: "Yahoo"},
+  			{y: 1.26, label: "Others"}
+  		]
+  	}]
+  });
+  chart.render();
+});
