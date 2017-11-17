5d5e479c34f979d20b6b44cbff5728fa17cc0c6e
Mahmoud Reyad <MahmoudReyad@fci.helwan.edu.eg>
2017-11-17 17:57:43 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   var chart = new CanvasJS.Chart("chartContainer", {
   	animationEnabled: true,
   	title: {
-
   		text: "Our Company Achivment"
   	},
   	data: [{
@@ -20,4 +19,53 @@ $(document).ready(function(){
   	}]
   });
   chart.render();
+
+  $('.gearcheck').on('click' , function(){
+     $('.coloroption').slideToggle(1000);
+  });
+  $('.coloroption ul li:first-of-type').on('click' , function(){
+
+  });
+
+// Change Theme Color On click
+$('.coloroption ul li').eq(0).css("background-color" , "#f00");
+
+$('.coloroption ul li').on("click" , function(){
+var y =   $(this).data('value') ;
+$("link[href*='theme']").attr("href" , y);
 });
+// Start Scroll Top
+  $(window).on("scroll" , function(){
+     if($(this).scrollTop() >= $('.features').offset().top ){
+      $('#scroll-top').fadeIn(500);
+      //console.log($(this).scrollTop());
+      // console.log($('.features').offset().top);
+    }
+    else {
+      $('#scroll-top').fadeOut(500);
+    }
+  });
+// onClick
+$('#scroll-top').on('click' , (function(){
+  $('body, html').animate({
+    scrollTop : 0,
+
+  } , 2000);
+}));
+new WOW().init();
+ $("body").niceScroll({
+cursorcolor:"#e74c3c",
+cursorwidth:"10px",
+});
+
+});
+// Loading Page
+$(window).on('load' ,(function(){
+  $('.loading-overlay .spinner').delay(1000).fadeOut(1000 , function(){
+    $('.loading-overlay').fadeOut(5000)
+    $('body').css("overflow" , "auto");
+
+  });
+
+$(this).remove();
+}));
